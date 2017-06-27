a7280b4f5a52c7ffd6dee07e16c041a6665b7392
unknown <hagydaniel@gmail.com>
2017-06-26 21:47:35 -0500

    Workingupdate

--- assets/javascript/food.js
+++ assets/javascript/food.js
@@ -1,21 +1,9 @@
-		//BARZZ api page = https://www.programmableweb.com/api/barzz
-		//BARZZ documentation = https://barzz.3scale.net/docs
-		//=====================================================================================================
+//BARZZ api page = https://www.programmableweb.com/api/barzz
+//BARZZ documentation = https://barzz.3scale.net/docs
+//================================================================================================
+	
+	var barzz = $(this).attr("data-name");
+	var queryURL = "http://echo-api.3scale.net/?user_key=dbd687c7ace8bf9a4dce0971129d8320" + barzz;
 
-		//function to diplay the appropriate content
-		function displayBarzzInfor(){
 
-		var barzz = $(this).attr("data-name");
-		var queryURL = "http://echo-api.3scale.net/?user_key=dbd687c7ace8bf9a4dce0971129d8320" + barzz;
-
-		// Create an AJAX for the specific bar that is being clicked by the user
-		$.ajax({
-			url: queryURL,
-			method: "GET"
-		}).done(function(respone){
-
-		})
-
-		}
-		
 
