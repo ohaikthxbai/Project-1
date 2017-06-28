43379d608c668bf5990865fd8c189f7c3d13dfa6
unknown <hagydaniel@gmail.com>
2017-06-28 13:54:05 -0500

    Zomato Update

--- assets/javascript/food.js
+++ assets/javascript/food.js
@@ -1,9 +1,18 @@
-//BARZZ api page = https://www.programmableweb.com/api/barzz
-//BARZZ documentation = https://barzz.3scale.net/docs
-//================================================================================================
-	
-	var barzz = $(this).attr("data-name");
-	var queryURL = "http://echo-api.3scale.net/?user_key=dbd687c7ace8bf9a4dce0971129d8320" + barzz;
+//URL + API key for Zomato.com
+//url = https://developers.zomato.com/api/v2.1/search?entity_id=292&entity_type=city&apikey=
+//API key = 858f948cc66e31e989a5e17f72be49e3
 
+// create var for HTML button
+	var btn = document.getElementById('btn');
+// Create click method with a function to request data via GET from Zomato
+	btn.addEventListener("click", function(){
+		var ourRequest = new XMLHttpRequest();
+		ourRequest.open('GET', "https://developers.zomato.com/api/v2.1/search?entity_id=292&entity_type=city&apikey=858f948cc66e31e989a5e17f72be49e3");
+	ourRequest.onload = function(){
+		var ourData = JSON.parse(ourRequest.reponseText);
+		
+	};
+	ourRequest.send();
+})
 
 
